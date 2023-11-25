0506c92e48d6c3d32ea06d1f5d9d0626acd17018
zwz <xmli688aa@gmail.com>
2023-11-25 22:11:55 +0800

    Stack综合练习 1.超出父控件允许显示，clipBehavior: Clip.none, 2.允许浮在父控件上，子组件使用Positioned组件，设置top，left，right，bottom位置 3.组件添加点击事件，使用GestureDetector组件包裹组件，添加手势方法

---
+++ lib/day02/09_Stack组件的使用.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  //1.Flutter启动需要执行runApp函数
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MYHomePage(),
+    );
+  }
+}
+
+class MYHomePage extends StatelessWidget {
+  const MYHomePage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Hello World")),
+      body: const HomePageContent(),
+      floatingActionButton:
+          FloatingActionButton(onPressed: () {}, child: Icon(Icons.add)),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
+
+class HomePageContent extends StatefulWidget {
+  const HomePageContent({super.key});
+
+  @override
+  State<HomePageContent> createState() => _HomePageContentState();
+}
+
+class _HomePageContentState extends State<HomePageContent> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      //超出父控件 允许显示
+      clipBehavior: Clip.none,
+      children: [
+        Container(
+          color: Colors.red,
+          width: 300,
+          height: 200,
+        ),
+        Image.asset("assets/images/headerIcon.jpg"),
+        Positioned(
+          bottom: -44,
+          left: 0,
+          right: 0,
+          child: Container(
+            padding: EdgeInsets.only(left: 20, right: 20),
+            color: Color.fromARGB(212, 19, 19, 19),
+            width: double.infinity,
+            height: 44,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "这个人头像挺不错的",
+                  style: TextStyle(fontSize: 24, color: Colors.white),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    print("点击了不错");
+                  },
+                  onLongPress: () {
+                    print("长按了不错图标");
+                  },
+                  child: Icon(
+                    Icons.favorite,
+                    color: Colors.red,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {
+                    print("点击了⭐️");
+                  },
+                  icon: Icon(
+                    Icons.star,
+                    color: Colors.green,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Text(
+                    "哈哈",
+                    style: TextStyle(color: Colors.red),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
