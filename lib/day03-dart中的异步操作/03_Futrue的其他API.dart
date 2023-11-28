fb52fc6ec37c8fc4c1042f2fd60c7e6865051041
zwz <xmli688aa@gmail.com>
2023-11-28 11:32:30 +0800

    Future的延迟执行任务

---
+++ lib/day03-dart中的异步操作/03_Futrue的其他API.dart
@@ -0,0 +1,23 @@
+void main(List<String> args) {
+  // Future(() {
+  //   return "哈哈";
+  // }).then((value) {
+  //   print(value);
+  // });
+
+  Future.value("哈哈哈").then((result) {
+    print(result);
+  });
+  Future.error("错误信息").catchError((value) {
+    print(value);
+  });
+  Future.delayed(Duration(seconds: 2), () {
+    print("延迟2s执行了任务");
+    return "hello fullter";
+  }).then((value) {
+    print("拿到了第一次返回的结果：$value");
+    return "hello world";
+  }).then((value) {
+    print("拿到了第二次返回的结果：$value");
+  });
+}
