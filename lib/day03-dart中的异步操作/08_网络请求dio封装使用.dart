22329905b2649333cc6fbe8d832aa4f2c2c98d60
zwz <xmli688aa@gmail.com>
2023-11-29 23:08:37 +0800

    网络请求框架dio的封装 1.BaseOptions中配置通用参数，如baseUrl,connectTimeout,headers 2.设置请求url和参数必传，method非必传，默认为get 3.添加默认的拦截器，请求前拦截onRequest，响应拦截onResponse，错误拦截onError

---
+++ lib/day03-dart中的异步操作/08_网络请求dio封装使用.dart
@@ -0,0 +1,72 @@
+
+import "package:my_app/service/http_request.dart";
+import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+void main() {
+  //1.Flutter启动需要执行runApp函数
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MYHomePage(),
+    );
+  }
+}
+
+class MYHomePage extends StatelessWidget {
+  const MYHomePage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("网络请求")),
+      body: const HomePageContent(),
+    );
+  }
+}
+
+class HomePageContent extends StatefulWidget {
+  const HomePageContent({super.key});
+
+  @override
+  State<HomePageContent> createState() => _HomePageContentState();
+}
+
+class _HomePageContentState extends State<HomePageContent> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    //发送网络请求
+    // //1创建dio
+    // final dio = Dio();
+    // dio.get("https://httpbin.org/get").then((res){
+    //   print(res);
+    // });
+    // dio.post("https://httpbin.org/post").then((res){
+    //   print(res);
+    // });
+    HttpRequest.request(url: "https://httpbin.org/get", params: {"name":"张三","age":11},
+      // headers: {"nameeee":"lilili"},
+      //   inter: InterceptorsWrapper(
+      // onResponse: (options,handler){
+      //   print("单独拦截响应：${options}");
+      //   return handler.next(options);
+      // })
+    ).then((value){
+      // print(value);
+    });
+    HttpRequest.postRequest(url: "https://httpbin.org/post", params: {"name":"zez"}).then((value){
+
+    });
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Text("hello world");
+  }
+}
---
+++ lib/service/http_request.dart
@@ -0,0 +1,69 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:my_app/service/config.dart';
+
+class HttpRequest {
+
+  static final BaseOptions baseOptions = BaseOptions(
+      baseUrl: HttpConfig.baseUrl,
+      connectTimeout: const Duration(seconds: HttpConfig.timeout),
+    //这里可以配置通用的headers 或者设置token
+    headers: {"Authorization":"commonAuthor"}
+  );
+  static final Dio dio = Dio(baseOptions);
+
+  static Future<T> request<T>(
+      {required String url,
+      required Map<String, dynamic> params,
+      String method = "get",
+        Interceptor? inter,Map<String,dynamic>? headers}) async {
+    //1.创建单独配置
+    final option = Options(method: method,headers: headers);
+
+    //全局拦截器
+    ///添加拦截器
+    Interceptor defaultInterceptor = InterceptorsWrapper(
+      onRequest: (options,handler) {
+        // print("请求前拦截触发");
+        // 打印请求的地址
+        print('Request Url: ${options.uri}');
+        // // 在请求前的拦截器中可以执行一些操作，例如添加通用的请求头
+        // options.headers['Custom-Header'] = 'Custom-Value';
+        return handler.next(options);// 继续请求
+      },
+      onResponse: (response,handler){
+        // print("结束请求拦截触发");
+        // 在请求后的拦截器中可以执行一些操作，例如日志记录
+        print('统一拦截响应: $response');
+        return handler.next(response); // 继续后续操作
+      },
+      onError: (error,handler) {
+        print('错误拦截: $error');
+        return handler.next(error);
+      },
+    );
+    List<Interceptor> inters = [defaultInterceptor];
+    //如果传过来的有拦截器 就添加到拦截器数组中
+    if(inter!= null){
+      inters.add(inter);
+    }
+    //统一添加到拦截器中
+    dio.interceptors.addAll(inters);
+
+    //2.发送网络请求
+    try {
+      Response response =
+          await dio.request(url, queryParameters: params, options: option);
+      return response.data;
+    } on DioException catch (e) {
+      return Future.error(e);
+    }
+  }
+  static Future<T> postRequest<T>(
+      {required String url,
+        required Map<String, dynamic> params,
+        Interceptor? inter,Map<String,dynamic>? headers}) async{
+    return request(url: url, params: params,method: "post",inter: inter,headers: headers);
+  }
+
+}
