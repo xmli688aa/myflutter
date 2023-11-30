eceb1e2f2160a98f76d463271a2ac6553bb7df3f
zwz <xmli688aa@gmail.com>
2023-11-30 22:18:33 +0800

    封装评分和分割线

---
+++ lib/douban/Widgets/dashed_line.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class ETDashedLine extends StatelessWidget {
+  final double containerSize ;
+  final Axis axis;
+  final double dashedWidth;
+  final double dashedHeight;
+  final int count;
+  final Color color;
+  ETDashedLine({
+    this.containerSize = 200,
+    this.axis = Axis.horizontal,
+    this.dashedWidth = 2,
+    this.dashedHeight = 2,
+    this.count = 15,
+    this.color = Colors.red,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: axis==Axis.horizontal? containerSize:dashedHeight,
+      height:axis==Axis.vertical? containerSize:dashedWidth,
+      child: ETDashedLineContent(axis: axis, dashedWidth: dashedWidth, dashedHeight: dashedHeight, count: count, color: color),
+    );
+  }
+}
+
+class ETDashedLineContent extends StatelessWidget {
+  final Axis axis;
+
+  final double dashedWidth;
+  final double dashedHeight;
+  final int count;
+  final Color color;
+
+  ETDashedLineContent({
+    required this.axis ,
+    required this.dashedWidth ,
+    required this.dashedHeight ,
+    required this.count ,
+    required this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Flex(
+      direction: axis,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: List.generate(this.count, (_) {
+        return SizedBox(
+          width: dashedWidth,
+          height: dashedHeight,
+          child: DecoratedBox(
+            decoration: BoxDecoration(color: color),
+          ),
+        );
+      }),
+    );
+  }
+}
