8086a64a050672cd8d4909687ec8053e0d7c2599
zwz <xmli688aa@gmail.com>
2023-12-04 16:13:16 +0800

    底部tabbar点击按钮会闪烁的问题

--- lib/douban/main/bottom_bar_item.dart
+++ lib/douban/main/bottom_bar_item.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class ETBottomBarItem extends BottomNavigationBarItem{
   ETBottomBarItem(String iconName,String title):super(
       label: title,
-      icon: Image.asset("assets/images/tabbar/${iconName}.png",width: 30,),
-      activeIcon: Image.asset("assets/images/tabbar/${iconName}_active.png",width: 30,)
+      icon: Image.asset("assets/images/tabbar/${iconName}.png",width: 32,gaplessPlayback: true,),
+      activeIcon: Image.asset("assets/images/tabbar/${iconName}_active.png",width: 32,gaplessPlayback: true,)
   );
 }
