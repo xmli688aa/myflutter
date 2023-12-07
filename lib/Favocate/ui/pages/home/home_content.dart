ac85c6c0b9354797a832d41bd279257c9588973a
zwz <xmli688aa@gmail.com>
2023-12-07 12:34:19 +0800

    1.FutureBuilder组件异步加载数据（有局限性：多次网络请求/有下拉刷新的不适合） 2.FutureBuilder的使用方法 有两个必传参数， 	1是future: JsonParse.getCategoryData(), 	2是builder：(ctx, snapShot) {}从snapShot中获取Future的数据 3.loading框组件CircularProgressIndicator()

--- lib/Favocate/ui/pages/home/home_content.dart
+++ lib/Favocate/ui/pages/home/home_content.dart
@@ -5,41 +5,32 @@ import 'package:my_app/day14_screenfit/extension/int_extension.dart';
 import '../../../core/model/category_model.dart';
 import 'category_item.dart';
 
-class HYHomeContent extends StatefulWidget {
+class HYHomeContent extends StatelessWidget {
   const HYHomeContent({Key? key}) : super(key: key);
 
   @override
-  State<HYHomeContent> createState() => _HYHomeContentState();
-}
-
-class _HYHomeContentState extends State<HYHomeContent> {
-  final List<HYCategoryModel> _categories = [];
-
-  @override
-  void initState() {
-    super.initState();
-    //获取json数据
-    JsonParse.getCategoryData().then((value) {
-      print(value);
-      setState(() {
-        _categories.addAll(value);
-      });
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-        padding: EdgeInsets.all(20.px),
-        itemCount: _categories.length,
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            childAspectRatio: 1.5,
-            mainAxisSpacing: 20.px,
-            crossAxisSpacing: 20.px),
-        itemBuilder: (ctx, index) {
-          return HYHomeCategoryItem(_categories[index]);
+    return FutureBuilder<List<HYCategoryModel>>(
+        future: JsonParse.getCategoryData(),
+        builder: (ctx, snapShot) {
+          if (!snapShot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          // print("FutureBuilder object");
+          if (snapShot.error != null) return const Center(child: Text("请求错误"));
+          // print("FutureBuilder object2");
+          final List<HYCategoryModel>? categories = snapShot.data;
+          return GridView.builder(
+              padding: EdgeInsets.all(20.px),
+              itemCount: categories?.length,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 1.5,
+                  mainAxisSpacing: 20.px,
+                  crossAxisSpacing: 20.px),
+              itemBuilder: (ctx, index) {
+                return HYHomeCategoryItem(categories![index]);
+              });
         });
   }
 }
-
