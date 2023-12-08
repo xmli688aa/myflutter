57cb19c2a67e1b335ce0964e16c225391a5229ca
zwz <xmli688aa@gmail.com>
2023-12-08 22:55:36 +0800

    1.美食列表页面 2.判断两个数组是否相等ListEquality().equals(prev, next)，需要导入包import 'package:collection/collection.dart'; 3.获取provider中的数据，使用Selector方法 Selector<HYMealViewModel, List<HYMealModel>>(       selector: (ctx, mealVM) {         return mealVM.meals             .where((meal) => meal.categories.contains(categoryModel.id))             .toList();       },       shouldRebuild: (prev, next) => !ListEquality().equals(prev, next),
          builder: (ctx, meals, child) {
            return ListView.builder(
                itemCount: meals.length,
                itemBuilder: (ctx, index) {
                  HYMealModel model = meals[index];
                  return Text("${model.title}");
                });
          },
        );

--- lib/Favorcate/ui/pages/meal/meal.dart
+++ lib/Favorcate/ui/pages/meal/meal.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'meal_content.dart';
 
 import '../../../core/model/category_model.dart';
 
@@ -12,12 +12,13 @@ class HYMealScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     // 获取参数
     final HYCategoryModel category = ModalRoute.of(context)?.settings.arguments as HYCategoryModel;
+    print(category.id);
     return Scaffold(
       appBar: AppBar(
         title: Text(category.title as String),
       ),
       body: Center(
-        child: Text("Meal List"),
+        child: HYMealContent(category),
       ),
     );
   }
---
+++ lib/Favorcate/ui/pages/meal/meal_content.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:collection/collection.dart';
+
+import 'package:my_app/Favorcate/core/model/category_model.dart';
+import 'package:my_app/Favorcate/core/model/meal_model.dart';
+import 'package:my_app/Favorcate/core/viewmodel/meal_view_model.dart';
+import 'package:provider/provider.dart';
+
+class HYMealContent extends StatelessWidget {
+  HYCategoryModel categoryModel;
+
+  HYMealContent(this.categoryModel, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    print(categoryModel.id);
+    return Selector<HYMealViewModel, List<HYMealModel>>(
+      selector: (ctx, mealVM) {
+        return mealVM.meals
+            .where((meal) => meal.categories.contains(categoryModel.id))
+            .toList();
+      },
+      shouldRebuild: (prev, next) => !ListEquality().equals(prev, next),
+
+      builder: (ctx, meals, child) {
+        return ListView.builder(
+            itemCount: meals.length,
+            itemBuilder: (ctx, index) {
+              HYMealModel model = meals[index];
+              return Text("${model.title}");
+            });
+      },
+    );
+  }
+}
+
+
+// class HYMealContent extends StatelessWidget {
+//   HYCategoryModel categoryModel;
+//
+//   HYMealContent(this.categoryModel, {Key? key}) : super(key: key);
+//   @override
+//   Widget build(BuildContext context) {
+//     print(categoryModel.id);
+//     return Consumer<HYMealViewModel>(builder:(ctx,mealVM,child){
+//       // List<HYMealModel> hyMealModels = [];
+//       // for(HYMealModel model in mealVM.meals){
+//       //   List categories = model.categories;
+//       //   if(categories.contains(categoryModel.id)){
+//       //     hyMealModels.add(model);
+//       //   }
+//       // }
+//       //上面的东西可以简写成下面的
+//       List<HYMealModel> hyMealModels = mealVM.meals.where((meal) => meal.categories.contains(categoryModel.id)).toList();
+//       print("${mealVM.meals}");
+//       return ListView.builder(
+//           itemCount: hyMealModels.length,
+//           itemBuilder: (ctx,index){
+//             HYMealModel mealModel = hyMealModels[index];
+//             return Text("${mealModel.title}");
+//           });
+//     });
+//   }
+// }
